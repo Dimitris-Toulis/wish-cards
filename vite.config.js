85b463957d8845fc2fd0a564d8904205ca7c54f3
Dimitris-Toulis <dimitritouli@gmail.com>
2025-10-30 23:27:15 +0200

    fix building and optimize

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,23 @@ import { defineConfig } from "vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig({
 	plugins: [cloudflare()],
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "index.html"),
-				create: resolve(__dirname, "create.html")
+	environments: {
+		client: {
+			build: {
+				rollupOptions: {
+					input: {
+						main: resolve(__dirname, 'index.html'),
+						create: resolve(__dirname, 'create.html'),
+						view: resolve(__dirname, 'v.html'),
+					},
+					output: {
+						manualChunks(id) {
+							if (id.includes("@tsparticles")) return "tsparticles"
+						}
+					}
+				},
 			}
 		}
 	}
