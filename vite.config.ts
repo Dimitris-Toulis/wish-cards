5c26b38b024794a827107ccba686f551f823abb6
Dimitris-Toulis <dimitritouli@gmail.com>
2025-10-31 14:13:33 +0200

    perf improvements

--- vite.config.ts
+++ vite.config.ts
@@ -11,16 +11,16 @@ export default defineConfig({
 			build: {
 				rollupOptions: {
 					input: {
-						main: resolve(__dirname, 'index.html'),
-						create: resolve(__dirname, 'create.html'),
-						view: resolve(__dirname, 'v.html'),
+						main: resolve(__dirname, "index.html"),
+						create: resolve(__dirname, "create.html"),
+						view: resolve(__dirname, "v.html")
 					},
 					output: {
 						manualChunks(id) {
-							if (id.includes("@tsparticles")) return "tsparticles"
+							if (id.includes("@tsparticles")) return "tsparticles";
 						}
 					}
-				},
+				}
 			}
 		}
 	}
